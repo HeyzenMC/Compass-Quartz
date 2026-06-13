2b1ab299e9ac7cdc214e0e3760e2ffde6c1872f5
HeyzenMC <christopher.herbig@googlemail.com>
2026-06-13 17:26:46 +0200

    Quartz sync: Jun 13, 2026, 5:26 PM

--- quartz.ts
+++ quartz.ts
@@ -1,12 +1,10 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import { componentRegistry } from "./quartz/components/registry"
+
+componentRegistry.setOptionOverrides("explorer", {
+  filterFn: (node) => node.slug !== "Sonstige" && !node.slug?.startsWith("Sonstige/"),
+})
 
 const config = await loadQuartzConfig()
 export default config
 export const layout = await loadQuartzLayout()
-
-import * as ExternalPlugin from "./.quartz/plugins";
-
-ExternalPlugin.Explorer({
-  title: "Navigation",
-  filterFn: (node) => node.slug !== "Sonstige" && !node.slug?.startsWith("Sonstige/"),
-});
